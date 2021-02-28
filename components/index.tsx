d6d85884e8bf6e5f26e8cc50a8eb18d0f724b5db
Nicolás Poore <nicolaspoore@gmail.com>
2021-02-28 19:33:09 -0300

    Moved AddPoints to root components folder

--- components/index.tsx
+++ components/index.tsx
@@ -1,5 +1,5 @@
 export { default as Header } from './Header';
 export { default as Banner } from './Banner';
+export { default as AddPoints } from './AddPoints';
 export { default as Products } from './Products/index';
-export { default as AddPoints } from './AddPoints/index';
 export { default as UserHistory } from './UserHistory/index';
