6288034a31cd40bbfed1d7a52a59863f710adde9
Nicolás Poore <nicolaspoore@gmail.com>
2021-03-01 18:11:09 -0300

    Transaction history responsiveness

--- components/UserHistory/HistoryItem.tsx
+++ components/UserHistory/HistoryItem.tsx
@@ -10,32 +10,38 @@ interface Props {
 
 const HistoryItem: React.FC<Props> = (props): JSX.Element => (
   <>
-    <Stack alignItems="center" direction="row" justify="space-between" py={5}>
-      <Stack direction="row" minWidth={80}>
-        <Image fallbackSrc={props.item.img.url} mr={4} src={props.item.img.hdUrl} width={24} />
+    <Stack alignItems="center" direction="row" justify="space-between" py={[0, 5]}>
+      <Stack direction="row" width={[28, 80]}>
+        <Image
+          display={['none', 'initial']}
+          fallbackSrc={props.item.img.url}
+          mr={4}
+          src={props.item.img.hdUrl}
+          width={24}
+        />
         <Stack justify="center">
-          <Text color="gray.400" fontSize="sm">
+          <Text color="gray.400" fontSize={['xs', 'sm']}>
             {props.item.category}
           </Text>
-          <Text fontSize="lg">{props.item.name}</Text>
+          <Text fontSize={['md', 'lg']}>{props.item.name}</Text>
         </Stack>
       </Stack>
       <Stack justify="center">
-        <Text color="gray.400" fontSize="sm">
+        <Text color="gray.400" fontSize={['xs', 'sm']}>
           {formatDate(props.item.createDate)}
         </Text>
-        <Text>Quantity: {props.item.qty ? formatNumbers(props.item.qty) : '1'}</Text>
+        <Text>Qty: {props.item.qty ? formatNumbers(props.item.qty) : '1'}</Text>
       </Stack>
-      <Stack alignItems="flex-end" minWidth={40}>
+      <Stack alignItems="flex-end" width={32}>
         <Stack direction="row">
-          <Text color={`${props.item.qty && 'gray.400'}`} fontSize={props.item.qty && 'sm'}>
-            Item price: {formatNumbers(props.item.cost)}
+          <Text color={`${props.item.qty && 'gray.400'}`} fontSize={props.item.qty && ['xs', 'sm']}>
+            Price: {formatNumbers(props.item.cost)}
           </Text>
           <Image fallbackSrc="/icons/coin.svg" src="/icons/coin.svg" width="25px" />
         </Stack>
         {props.item.qty && (
           <Stack direction="row">
-            <Text>Total price: {formatNumbers(props.item.cost * props.item.qty)}</Text>
+            <Text>Total: {formatNumbers(props.item.cost * props.item.qty)}</Text>
             <Image fallbackSrc="/icons/coin.svg" src="/icons/coin.svg" width="25px" />
           </Stack>
         )}
